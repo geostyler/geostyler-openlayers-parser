250859d803b0d27c363f1b35fddc4bd3694e509b
Christian Mayer <chris@meggsimum.de>
2018-06-14 09:23:53 +0200

    Update dependencies
    This updates the dependencies and devDependencies and the needed changes
    (especially due to our internal style model).

--- data/styles/line_simpleline.ts
+++ data/styles/line_simpleline.ts
@@ -2,8 +2,10 @@ import { Style } from 'geostyler-style';
 
 const lineSimpleLine: Style = {
   type: 'Line',
+  name: 'OL Style',
   rules: [
     {
+      name: 'OL Style Rule',
       symbolizer: {
         kind: 'Line',
         color: '#000000',
--- data/styles/polygon_transparentpolygon.ts
+++ data/styles/polygon_transparentpolygon.ts
@@ -2,8 +2,10 @@ import { Style } from 'geostyler-style';
 
 const polygonTransparentPolygon: Style = {
   type: 'Fill',
+  name: 'OL Style',
   rules: [
     {
+      name: 'OL Style Rule',
       symbolizer: {
         kind: 'Fill',
         color: '#000080',
