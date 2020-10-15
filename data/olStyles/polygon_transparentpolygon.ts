9b7c94e5175406b98816ad746d1e6ab122238bc7
rdewit <git@rdewit.net>
2020-10-15 16:19:22 +1100

    Update tests to reflect new capabilities

--- data/olStyles/polygon_transparentpolygon.ts
+++ data/olStyles/polygon_transparentpolygon.ts
@@ -4,7 +4,9 @@ import OlStyleFill from 'ol/style/Fill';
 
 const olPolygonTransparentPolygon = new OlStyle({
   stroke: new OlStyleStroke({
-    color: '#FFFFFF'
+    color: 'rgba(255,255,255,0.7)',
+    lineDash: [10, 15],
+    width: 2
   }),
   fill: new OlStyleFill({
     color: 'rgba(0,0,128,0.5)'
