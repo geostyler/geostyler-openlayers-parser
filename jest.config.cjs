4da75810f52cc62a99b9c84532f16b1b39d10475
Andreas Schmitz <schmitz@terrestris.de>
2024-06-19 10:55:17 +0200

    feat: use esm build & update geostyler-style
    BREAKING CHANGE: You may need to adapt your imports, your bundler
    config or other configs when using this module to make sure your
    tooling knows this is now an esm module.

--- jest.config.cjs
+++ jest.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
     '^.+\\.(ts|js)$': '<rootDir>/node_modules/babel-jest'
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(ol|color-name|color-space|color-rgba|color-parse|@terrestris/*)/)'
+    'node_modules/(?!(ol|color-name|geostyler-style|color-space|color-rgba|color-parse|@terrestris/*)/)'
   ],
   testRegex: '/src/.*\\.spec.ts$',
   collectCoverageFrom: [
