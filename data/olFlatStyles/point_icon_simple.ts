c3626beedb039801543b6b4bbea717ac3a421c88
jansule <suleiman@terrestris.de>
2025-05-13 14:36:46 +0200

    feat: add OlFlatStyleParser

---
+++ data/olFlatStyles/point_icon_simple.ts
@@ -0,0 +1,9 @@
+import { FlatStyle } from 'ol/style/flat';
+
+const pointSimplePoint: FlatStyle = {
+  'icon-src': 'https://avatars1.githubusercontent.com/u/1849416?s=460&v=4',
+  'icon-rotation': 45,
+  'icon-offset': [10, 20]
+};
+
+export default pointSimplePoint;
