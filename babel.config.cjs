85a0c3685226000ea15bc5d57ba46a9134bc9335
Olivia <olivia.guyot@camptocamp.com>
2025-06-05 10:15:47 +0200

    chore(deps): remove deprecated babel packages

--- babel.config.cjs
+++ babel.config.cjs
@@ -2,9 +2,5 @@ module.exports = {
   'presets': [
     '@babel/env',
     '@babel/preset-typescript'
-  ],
-  'plugins': [
-    '@babel/proposal-class-properties',
-    '@babel/proposal-object-rest-spread'
   ]
 };
