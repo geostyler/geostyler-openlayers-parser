6304a9d1f8383675f97f850242702a8b93a1bdcb
Laure-Hélène Bruneton <laure-helene.bruneton@camptocamp.com>
2025-08-21 17:16:26 +0200

    feat(flat-style): slash, backslash, vertline, horline

---
+++ data/olFlatStyles/point_simplevertline.ts
@@ -0,0 +1,9 @@
+import { FlatStyle } from 'ol/style/flat';
+
+const pointSimpleVertLine: FlatStyle = {
+  'shape-points': 2,
+  'shape-fill-color': '#FF0000',
+  'shape-radius': 6,
+};
+
+export default pointSimpleVertLine;
