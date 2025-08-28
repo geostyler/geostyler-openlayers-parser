4e71e8f4fc5e30f9c2ee66939fe0fe01638f0032
Laure-Hélène Bruneton <laure-helene.bruneton@camptocamp.com>
2025-08-28 11:31:26 +0200

    feat(flat-write): scale denominators to resolution filters

--- data/olFlatStyles/filter_resolution.ts
+++ data/olFlatStyles/filter_resolution.ts
@@ -1,12 +1,12 @@
 import { FlatStyleLike } from 'ol/style/flat';
 
+const dpi = 25.4 / 0.28;
+const inchesPerMeter = 39.37;
+const scale = ['*', ['resolution'], inchesPerMeter * dpi];
+
 const filterSimpleFilter: FlatStyleLike = [
   {
-    filter: [
-      '>=',
-      ['resolution'],
-      1000
-    ],
+    filter: ['>=', scale, 1000 * inchesPerMeter * dpi],
     style: {
       'circle-radius': 10,
       'circle-fill-color': '#FF0000'
