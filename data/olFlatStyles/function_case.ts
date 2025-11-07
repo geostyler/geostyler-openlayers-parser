bd591ad65313c22ee14b17f48eb16e75e13df6c9
Laure-Hélène Bruneton <laure-helene.bruneton@camptocamp.com>
2025-11-07 15:48:11 +0100

    test(flat): typo in case brackets

--- data/olFlatStyles/function_case.ts
+++ data/olFlatStyles/function_case.ts
@@ -2,7 +2,7 @@ import { FlatStyle } from 'ol/style/flat';
 
 const functionCase: FlatStyle = {
   'circle-fill-color': '#FF0000',
-  'circle-radius': ['case', ['<=', ['get', 'population'], 50000, 2, ['<=', ['get', 'population'], 1000000], 5, 10],]
+  'circle-radius': ['case', ['<=', ['get', 'population'], 50000], 2, ['<=', ['get', 'population'], 1000000], 5, 10]
 };
 
 export default functionCase;
